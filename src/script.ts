3c92f1a31b2aca3eb9527f127fb36d0adb68fb5c
Patrick van Bergen <patrick.vanbergen@gmail.com>
2025-10-18 11:09:54 +0200

    view

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 import { calculateDistance, calculatePieceCircleIntersections, calculatePiecePieceIntersection } from "./math";
 import type { Circle, Diamond, LinePiece } from "./types";
+import { drawCircles, drawDiamonds, drawLinePieces } from "./view";
 
 function createHorizontalLines(count: number) {
     const linePieces: LinePiece[] = [];
@@ -69,8 +70,6 @@ function createSlightDescendingLines() {
     ]
 }
 
-
-
 function calculateRadius(piece1: LinePiece, piece2: LinePiece) {
     const intersectionPoint = calculatePiecePieceIntersection(piece1, piece2);
     const centerPoint = {x: 2/8, y: 2/8}
@@ -134,49 +133,3 @@ export function run() {
 
 }
 
-function drawLinePieces(linePieces: LinePiece[], view: HTMLElement) {
-    for (const piece of linePieces) {
-        const l = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        l.setAttribute("x1", project(piece.a.x));
-        l.setAttribute("y1", project(piece.a.y));
-        l.setAttribute("x2", project(piece.b.x));
-        l.setAttribute("y2", project(piece.b.y));
-        l.setAttribute("stroke", "#000000");
-        l.setAttribute("stroke-width", "1");
-        view.append(l)
-    }
-}
-
-function drawCircles(circles: Circle[], view: HTMLElement) {
-    for (const circle of circles) {
-        const c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        c.setAttribute("r", project(circle.radius, false));
-        c.setAttribute("cx", project(circle.center.x));
-        c.setAttribute("cy", project(circle.center.y));
-        c.setAttribute("stroke", "#000000");
-        c.setAttribute("fill", "none");
-        c.setAttribute("stroke-width", "1");
-        view.append(c)
-    }
-}
-
-function drawDiamonds(diamonds: Diamond[], view: HTMLElement) {
-    for (const diamond of diamonds) {
-        const p = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-        p.setAttribute("points", [
-            [project(diamond.top.x), project(diamond.top.y)].join(","),
-            [project(diamond.right.x), project(diamond.right.y)].join(","),
-            [project(diamond.bottom.x), project(diamond.bottom.y)].join(","),
-            [project(diamond.left.x), project(diamond.left.y)].join(","),
-        ].join(" "));
-        p.setAttribute("stroke", "#000000");
-        p.setAttribute("fill", "none");
-        p.setAttribute("stroke-width", "1");
-        view.append(p)
-    }
-}
-
-function project(ordinate: number, useOffset: boolean = true): string {
-    const offset = useOffset ? 10 : 0;
-    return "" + (ordinate * 500 + offset)
-}
---
+++ src/view.ts
@@ -0,0 +1,46 @@
+export function drawLinePieces(linePieces: LinePiece[], view: HTMLElement) {
+    for (const piece of linePieces) {
+        const l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        l.setAttribute("x1", project(piece.a.x));
+        l.setAttribute("y1", project(piece.a.y));
+        l.setAttribute("x2", project(piece.b.x));
+        l.setAttribute("y2", project(piece.b.y));
+        l.setAttribute("stroke", "#000000");
+        l.setAttribute("stroke-width", "1");
+        view.append(l)
+    }
+}
+
+export function drawCircles(circles: Circle[], view: HTMLElement) {
+    for (const circle of circles) {
+        const c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+        c.setAttribute("r", project(circle.radius, false));
+        c.setAttribute("cx", project(circle.center.x));
+        c.setAttribute("cy", project(circle.center.y));
+        c.setAttribute("stroke", "#000000");
+        c.setAttribute("fill", "none");
+        c.setAttribute("stroke-width", "1");
+        view.append(c)
+    }
+}
+
+export function drawDiamonds(diamonds: Diamond[], view: HTMLElement) {
+    for (const diamond of diamonds) {
+        const p = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        p.setAttribute("points", [
+            [project(diamond.top.x), project(diamond.top.y)].join(","),
+            [project(diamond.right.x), project(diamond.right.y)].join(","),
+            [project(diamond.bottom.x), project(diamond.bottom.y)].join(","),
+            [project(diamond.left.x), project(diamond.left.y)].join(","),
+        ].join(" "));
+        p.setAttribute("stroke", "#000000");
+        p.setAttribute("fill", "none");
+        p.setAttribute("stroke-width", "1");
+        view.append(p)
+    }
+}
+
+function project(ordinate: number, useOffset: boolean = true): string {
+    const offset = useOffset ? 10 : 0;
+    return "" + (ordinate * 500 + offset)
+}
